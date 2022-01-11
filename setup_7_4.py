71472f92755e459565144dc2fb675174a025d2b4
Shin-jonghwan <62974484+Shin-jongwhan@users.noreply.github.com>
2022-01-11 10:32:59 +0900

    Update setup_7_4.py

--- setup_7_4.py
+++ setup_7_4.py
@@ -1,4 +1,4 @@
-# cmd에서 경로 변경 후 python [this script] install이라고 입력
+# cmd에서 경로 변경 후 python [this script] build_ext --inplace이라고 입력
 #from distutils.core import setup
 #from distutils.extension import Extension
 from setuptools import setup        # setuptools는 distutils의 최신 버전
